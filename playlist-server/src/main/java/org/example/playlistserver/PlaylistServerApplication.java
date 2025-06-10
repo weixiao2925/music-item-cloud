cdd932cf144625e6e10cc36f57a03670037391f4
weixiao2925 <weixiao2925@gmail.com>
2025-06-10 20:36:55 +0800

    为聚合、歌单、歌手和歌曲服务添加服务发现功能

--- playlist-server/src/main/java/org/example/playlistserver/PlaylistServerApplication.java
+++ playlist-server/src/main/java/org/example/playlistserver/PlaylistServerApplication.java
@@ -2,8 +2,10 @@ package org.example.playlistserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PlaylistServerApplication {
 
     public static void main(String[] args) {
